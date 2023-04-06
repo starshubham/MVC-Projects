defa34c9516a3f617fbb9dc010364d3c4a1b9711
Shubham Seth <shubhamsth1234@gmail.com>
2023-04-06 09:22:45 +0530

    Added MyFirstApp and ActionResultAssignment-1 Projects

---
+++ ActionResultAssignment-1/ActionResultAssginment/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ActionResultAssginment.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home/Index
+        public ActionResult Index(string arg)
+        {
+            //In case of 'sample', return pdf file
+            if (arg == "sample")
+            {
+                return File("~/Sample.pdf", "application/pdf");
+            }
+
+            //In case of 'gotoabout', return redirect to About action method
+            else if (arg == "gotoabout")
+            {
+                return RedirectToAction("About");
+            }
+
+            //In case of 'login', return 'Login' view
+            else if (arg == "login")
+            {
+                return View("Login");
+            }
+
+            //Otherwise, default message
+            else
+            {
+                return Content("You entered:  " + arg);
+            }
+        }
+
+        // GET: Home/About
+        public ActionResult About()
+        {
+            return Content("About content here");
+        }
+    }
+}
+
---
+++ MyFirstApp/MyFirstApp/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login(string Username, string Password)
+        {
+            if (Username == "admin" && Password == "manager")
+                return RedirectToAction("Dashboard", "Admin");  // (Action name, Controller name)
+            else
+                return RedirectToAction("InvalidLogin");
+        }
+
+        public ActionResult InvalidLogin()
+        {
+            return View();
+        }
+    }
+}
