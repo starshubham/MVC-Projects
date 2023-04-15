2ab9478e9dc89c53d5eb3570492640b825e8975e
Shubham Seth <shubhamsth1234@gmail.com>
2023-04-15 21:42:05 +0530

    [shubham] updated EFDbFirstArroachExample project

--- EFDbFirstApproachExample/EFDbFirstApproachExample/Controllers/ProductsController.cs
+++ EFDbFirstApproachExample/EFDbFirstApproachExample/Controllers/ProductsController.cs
@@ -10,12 +10,73 @@ namespace EFDbFirstApproachExample.Controllers
     public class ProductsController : Controller
     {
         // GET: Products
-        public ActionResult Index(string search = "")
+        public ActionResult Index(string search = "", string SortColumn = "ProductName", string IconClass="fa-sort-asc", int PageNo = 1)
         {
             EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
             //List<Product> products = db.Products.Where(temp => temp.CategoryID == 1 && temp.Price >= 50000).ToList();
             ViewBag.search = search;
             List<Product> products = db.Products.Where(temp => temp.ProductName.Contains(search)).ToList();
+
+            //Sorting
+            ViewBag.SortColumn = SortColumn;
+            ViewBag.IconClass = IconClass;
+            if(ViewBag.SortColumn == "ProductID")
+            {
+                if (ViewBag.IconClass == "fa-sort-asc")
+                    products = products.OrderBy(temp => temp.ProductID).ToList();
+                else
+                    products = products.OrderByDescending(temp => temp.ProductID).ToList();
+            }
+            else if (ViewBag.SortColumn == "ProductName")
+            {
+                if (ViewBag.IconClass == "fa-sort-asc")
+                    products = products.OrderBy(temp => temp.ProductName).ToList();
+                else
+                    products = products.OrderByDescending(temp => temp.ProductName).ToList();
+            }
+            else if (ViewBag.SortColumn == "Price")
+            {
+                if (ViewBag.IconClass == "fa-sort-asc")
+                    products = products.OrderBy(temp => temp.Price).ToList();
+                else
+                    products = products.OrderByDescending(temp => temp.Price).ToList();
+            }
+            else if (ViewBag.SortColumn == "DateOfPurchase")
+            {
+                if (ViewBag.IconClass == "fa-sort-asc")
+                    products = products.OrderBy(temp => temp.DateOfPurchase).ToList();
+                else
+                    products = products.OrderByDescending(temp => temp.DateOfPurchase).ToList();
+            }
+            else if (ViewBag.SortColumn == "AvailabilityStatus")
+            {
+                if (ViewBag.IconClass == "fa-sort-asc")
+                    products = products.OrderBy(temp => temp.AvailabilityStatus).ToList();
+                else
+                    products = products.OrderByDescending(temp => temp.AvailabilityStatus).ToList();
+            }
+            else if (ViewBag.SortColumn == "CategoryID")
+            {
+                if (ViewBag.IconClass == "fa-sort-asc")
+                    products = products.OrderBy(temp => temp.CategoryID).ToList();
+                else
+                    products = products.OrderByDescending(temp => temp.CategoryID).ToList();
+            }
+            else if (ViewBag.SortColumn == "BrandID")
+            {
+                if (ViewBag.IconClass == "fa-sort-asc")
+                    products = products.OrderBy(temp => temp.BrandID).ToList();
+                else
+                    products = products.OrderByDescending(temp => temp.BrandID).ToList();
+            }
+
+            /* Paging */
+            int NoOfRecordsPerPage = 5;
+            int NoOfPages = Convert.ToInt32(Math.Ceiling(Convert.ToDouble(products.Count) / Convert.ToDouble(NoOfRecordsPerPage)));
+            int NoOfRecordsToSkip = (PageNo - 1) * NoOfRecordsPerPage;
+            ViewBag.PageNo = PageNo;
+            ViewBag.NoOfPages = NoOfPages;
+            products = products.Skip(NoOfRecordsToSkip).Take(NoOfRecordsPerPage).ToList();
             return View(products);
         }
 
@@ -24,6 +85,73 @@ namespace EFDbFirstApproachExample.Controllers
             EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
             Product p = db.Products.Where(temp => temp.ProductID == id).FirstOrDefault();
             return View(p);
+        }
+
+        public ActionResult Create()
+        {
+            EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
+            ViewBag.Categories = db.Categories.ToList();
+            ViewBag.Brands = db.Brands.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product p)
+        {
+            EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
+            if(Request.Files.Count >= 1)
+            {
+                var file = Request.Files[0];
+                var imgBytes = new Byte[file.ContentLength];
+                file.InputStream.Read(imgBytes, 0, file.ContentLength);
+                var base64String = Convert.ToBase64String(imgBytes, 0, imgBytes.Length);
+                p.Photo = base64String;
+            }
+            db.Products.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(long id)
+        {
+            EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
+            Product existingProduct = db.Products.Where(temp => temp.ProductID == id).FirstOrDefault();
+            ViewBag.Categories = db.Categories.ToList();
+            ViewBag.Brands = db.Brands.ToList();
+            return View(existingProduct);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product p)
+        {
+            EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
+            Product existingProduct = db.Products.Where(temp => temp.ProductID == p.ProductID).FirstOrDefault();
+            existingProduct.ProductName = p.ProductName;
+            existingProduct.Price = p.Price;
+            existingProduct.DateOfPurchase = p.DateOfPurchase;
+            existingProduct.CategoryID = p.CategoryID;
+            existingProduct.BrandID = p.BrandID;
+            existingProduct.AvailabilityStatus = p.AvailabilityStatus;
+            existingProduct.Active = p.Active;
+            db.SaveChanges();
+            return RedirectToAction("Index", "Products");
+        }
+
+        public ActionResult Delete(long id)
+        {
+            EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
+            Product existingProduct = db.Products.Where(temp => temp.ProductID == id).FirstOrDefault();
+            return View(existingProduct);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(long id, Product p)
+        {
+            EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
+            Product existingProduct = db.Products.Where(temp => temp.ProductID == id).FirstOrDefault();
+            db.Products.Remove(existingProduct);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Products");
         }
     }
 }
